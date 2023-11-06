329b60609a6928625725403845ba3bf08a29840f
Boniface <Cheruiyotbony@gmail.com>
2023-11-06 05:47:23 +0300

    final commit all chnages

--- Src/index.js
+++ Src/index.js
@@ -1,6 +1,4 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    
-    // Get references to HTML elements
+// Get references to HTML elements
 const movieSearchBox = document.getElementById('movie-search-box');
 const searchList = document.getElementById('search-list');
 const resultGrid = document.getElementById('result-grid');
@@ -13,10 +11,6 @@ async function loadMovies(searchTerm){
     console.log(data.Search); // Logging the movie search results to the console
     if(data.Response == "True") displayMovieList(data.Search);
 }
-
-
-
-
 
 // Function to initiate movie search
 function findMovies(){
@@ -97,5 +91,3 @@ window.addEventListener('click', (event) => {
         searchList.classList.add('hide-search-list');
     }
 });
-})
-
